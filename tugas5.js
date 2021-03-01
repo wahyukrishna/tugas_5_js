9e92a9e1f920f22dedae2578eb9f57a571b4730b
Wahyu Krishna <hanzo180895@gmail.com>
2021-03-01 12:06:38 +0700

    Tugas 5 JS
    Tugas Niomic

---
+++ tugas5.js
@@ -0,0 +1,17 @@
+var i=1;
+const loop = () => {
+    // for(i=1;i<=20;i++){
+    //     if(i%3==0)
+    //     console.log(i)
+    // }
+    while(i<=20){
+        if(i%3==0)
+        console.log(i)
+        i++
+        
+    }
+
+}
+
+loop();
+
